4a7134ddb04e909c65f7e46b8bfea5921f80afed
Oblivion Plain <45317380+oblivionplain@users.noreply.github.com>
2021-10-19 12:21:34 -0400

    Remove commented code

--- canvas.js
+++ canvas.js
@@ -32,40 +32,8 @@ const pad = document.getElementById("inputPad");
 
 window.addDraggableEvent(pad);
 
-/*let padDragging = {
-    enabled: false,
-    x: 0,
-    y: 0,
-    realX: 0,
-    realY: 0
-};
-
-function lerp(a, b, x) {
-    return a + (b - a) * x;
-}
-
-setInterval(function updateDragables() {
-    if (padDragging.enabled) {
-        pad.style.top = `${padDragging.y}px`;
-        pad.style.left = `${padDragging.x}px`;
-        padDragging.x = lerp(padDragging.x, padDragging.realX, .05);
-        padDragging.y = lerp(padDragging.y, padDragging.realY, .05);
-    }
-}, 10);
-
-document.body.addEventListener("mousedown", function mouseDownEvent(event) {
-    if (event.target.id === "inputPad") {
-        padDragging.enabled = true;
-    }
-});
-
-document.body.addEventListener("mouseup", function mouseDownEvent(event) {
-    padDragging.enabled = false;
-});*/
-
+// Sliding top
 document.body.addEventListener("mousemove", function mouseMoveEvent(event) {
-    //padDragging.realX = event.clientX;
-    //padDragging.realY = event.clientY;
     if (event.clientY < 75) {
         top.style.top = "0px";
     } else {
